effc6549f8caf535647148e907a7837242f6b5d7
Nic Jackson <jackson.nic@gmail.com>
2017-11-01 06:36:56 +0000

    Updated build config

--- startNomad.sh
+++ startNomad.sh
@@ -9,7 +9,7 @@ fi
 
 # Set IP Address in config
 sed "s/##HOST_IP##/${IP_ADDRESS}/g" < nomad_config.hcl.tmpl > nomad.hcl
-echo "Discovered IP Address: ${IP_ADDRESS}"
+#echo "Discovered IP Address: ${IP_ADDRESS}"
 
 # Create logs folder if needed
 if [ ! -d "${HOME}/log" ]; then
@@ -17,16 +17,16 @@ if [ ! -d "${HOME}/log" ]; then
 fi
 
 # Start Consul
-echo "Starting Consul, redirecting logs to $HOME/log/consul.log"
+#echo "Starting Consul, redirecting logs to $HOME/log/consul.log"
 sudo -b HOST_IP=${HOST_IP} nohup consul agent -dev -bind ${IP_ADDRESS} -dns-port 53 -client ${IP_ADDRESS} >~/log/consul.log 2>&1
 
 # Start Nomad
-echo "Starting Nomad, redirecting logs to $HOME/log/nomad.log"
+#echo "Starting Nomad, redirecting logs to $HOME/log/nomad.log"
 nohup nomad agent --config=nomad.hcl >~/log/nomad.log 2>&1 &
 
 # Set Nomad environment variable
-echo ""
-echo "You can set the following environment variables"
+#echo ""
+#echo "You can set the following environment variables"
 echo "export NOMAD_ADDR=http://${IP_ADDRESS}:4646"
 echo "export CONSUL_HTTP_ADDR=http://${IP_ADDRESS}:8500"
 echo "export FAAS_GATEWAY=http://${IP_ADDRESS}:8080"
