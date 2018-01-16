1c43877a68e6a6186b08e33c041138ab27a81f67
Nic Jackson <jackson.nic@gmail.com>
2018-01-16 10:59:40 +0000

    Added github release to goreleaser

---
+++ travis_deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then 
+  cd $GOPATH/src/github.com/hashicorp/faas-nomad;
+  docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" quay.io; 
+  goreleaser --rm-dist
+fi
